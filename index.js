86b762b3d1650339363ccaf9f034a8ef37777f33
SanchezYoan <yoan30470@hotmail.fr>
2023-04-23 20:25:08 +0200

    ending

--- index.js
+++ index.js
@@ -1,178 +1,260 @@
-
- // Cours DOM
-
-
-//  Selectors
-
-// Pointe un balise dans le chemin spécifié
-
-// document.querySelector('h4').style.background = "orange";
-
-// const baliseHTML = document.querySelector('h4').style.background = "orange";
-
-// console.log(baliseHTML)
+// SELECTEURS
+// document.querySelector("h4").style.background = "yellow";
+// const baliseHTML = document.querySelector("h4");
 
 // Click event
-
-const questionContainer = document.querySelector(".click-event")
-const btn1 = document.querySelector('#btn-1')
-const btn2 = document.querySelector('#btn-2')
-const response = document.querySelector('p')
-// console.log("questionContainer");
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
 
 questionContainer.addEventListener("click", () => {
-    questionContainer.classList.toggle("question-click")
+  questionContainer.classList.toggle("question-clicked");
 });
 
-// Ajouter une Class
+btn1.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "green";
+});
 
-btn1.addEventListener('click', () => {
-    // console.log("Click !")
-    response.classList.add('show-response') 
-    response.style.background = "red"
-    
-})
-btn2.addEventListener('click', () => {
-    // console.log("Click !")
-    response.classList.add('show-response') 
-    response.style.background = "blue";
+btn2.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "red";
+});
 
-})
+/* <div> > #id > .class > baliseHTML */
 
-
-{/* <div>  > #id > .class > baliseHTML */}
-
-// --------------------------------------------------------
-
-// Mouse events
-
-const mousemove = document.querySelector('.mousemove');
-// console.log(mousemove);
+//--------------------------------------------------
+// Mouse Events
+const mousemove = document.querySelector(".mousemove");
 
 window.addEventListener("mousemove", (e) => {
-    // console.log(e)
-    mousemove.style.left = e.pageX + "px";
-    mousemove.style.top = e.pageY + "px";
-})
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
 window.addEventListener("mousedown", () => {
-    // console.log("test");
-    mousemove.style.transform = "scale(2) translate(-25%, -25%)";
-})
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+});
 
 window.addEventListener("mouseup", () => {
-    // console.log("test");
-    mousemove.style.transform = "scale(1) translate(-50%, -50%)";
-    mousemove.style.border = "2px solid teal";
-})
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+  mousemove.style.border = "2px solid teal";
+});
 
-questionContainer.addEventListener('mouseenter', () => {
-    questionContainer.style.background = "rgba(0, 0, 0, 0.6)"
-})
+questionContainer.addEventListener("mouseenter", () => {
+  questionContainer.style.background = "rgba(0,0,0,0.6)";
+});
 
-questionContainer.addEventListener('mouseout', () => {
-    questionContainer.style.background = "blue"
-})
+questionContainer.addEventListener("mouseout", () => {
+  questionContainer.style.background = "pink";
+});
 
-response.addEventListener('mouseover', () => {
-    response.style.transform = "rotate(2deg)";
-})
+response.addEventListener("mouseover", () => {
+  response.style.transform = "rotate(2deg)";
+});
 
-// -------------------------------------------------------------------
-
-// Keypress
+//---------------------------------------------------
+// KeyPress event
 
 const keypressContainer = document.querySelector(".keypress");
 const key = document.getElementById("key");
 
 const ring = (key) => {
-    const audio = new Audio();
-    audio.src = key + ".mp3";
-    audio.play();
+  const audio = new Audio();
+  audio.src = key + ".mp3";
+  audio.play();
 };
 
 document.addEventListener("keypress", (e) => {
-    // console.log(e.key)
-    key.textContent = e.key
+  key.textContent = e.key;
 
-    if(e.key === "j") {
-        keypressContainer.style.background = "pink";
-    } else if (e.key === "h") {
-        keypressContainer.style.background = "green";
-    } else {
-        keypressContainer.style.background = "black";    
-    }
+  if (e.key === "j") {
+    keypressContainer.style.background = "pink";
+  } else if (e.key === "h") {
+    keypressContainer.style.background = "teal";
+  } else {
+    keypressContainer.style.background = "red";
+  }
+  if (e.key === "z") ring(e.key);
+});
 
-    if (e.key === "z") ring(e.key);
-    
-})
-// -----------------------------------------------------------
-
-// Scroll event 
+//-------------------------------------------------
+// Scroll Event
 
 const nav = document.querySelector("nav");
+
 window.addEventListener("scroll", () => {
+  if (window.scrollY > 120) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = "-50px";
+  }
+});
 
-    if (window.scrollY > 120 ) {
-        nav.style.transition = "0.5s";
-        nav.style.top = 0;
-    } else {
-        nav.style.top = "-50px"
-    }
-})
-
-// ------------------------------------------------------------
-
-// Form event 
- 
+//--------------------------------------------------
+// Form Events
 const inputName = document.querySelector('input[type="text"]');
 const select = document.querySelector("select");
 const form = document.querySelector("form");
-
 let pseudo = "";
 let language = "";
 
 inputName.addEventListener("input", (e) => {
-    pseudo = e.target.value;
+  pseudo = e.target.value;
 });
 
 select.addEventListener("input", (e) => {
-    language = e.target.value;
+  language = e.target.value;
 });
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-
-    if (cgv.checked) {
-        // Affiche le contenu des variables
-        document.querySelector("form > div").innerHTML = `
-            <h3>Pseudo : ${pseudo}</h3>
-            <h4>Languages préféré : ${language}</h4>
-        `;
-    } else {
-        alert("Veuillez acceptez les CGV");
-    }
+  if (cgv.checked) {
+    document.querySelector("form > div").innerHTML = `
+      <h3>Pseudo : ${pseudo}</h3>
+      <h4>Langage préféré : ${language}</h4>
+    `;
+  } else {
+    alert("Veuillez accepter les CGV");
+  }
 });
 
-// ------------------------------------------------------------
-
+//------------------------------------------------
 // Load event
-
-// appelé des fonctions au chargement de la page
 window.addEventListener("load", () => {
-    // console.log("Document chargé !")
+  // console.log("Document Chargé !");
 });
 
-// ------------------------------------------------------------
-
-//  ForEach 
-
-// selectionner tous les éléments de la class
-const boxes = document.querySelectorAll('.box')
-
+//------------------------------------------------
+// ForEach
+// const boxes = document.getElementsByClassName("box");
+const boxes = document.querySelectorAll(".box");
 
 boxes.forEach((box) => {
-    box.addEventListener("click", (e) => {
-        e.target.style.transform = "scale(0.7)"
-    })
+  box.addEventListener("click", (e) => {
+    box.style.transform = "scale(0.7)";
+  });
+});
+
+//------------------------------------------------
+// addEventListener Vs onclick
+// document.body.onclick = function() {
+//   console.log("Scroll !");
+// };
+
+// Bubbling => fin (de base l'eventlistener est paramétré en mode Bublbing)
+document.body.addEventListener("click", () => {
+  console.log("click 1 !");
+});
+
+// Usecapture
+document.body.addEventListener(
+  "click",
+  () => {
+    console.log("click 2 !");
+  },
+  true
+);
+
+// https://gomakethings.com/what-is-that-third-argument-on-the-vanilla-js-addeventlistener-method-and-when-do-you-need-it/
+
+//-------------------------------------------------
+// Stop propagation
+// questionContainer.addEventListener("click", (e) => {
+//   alert("Test !");
+//   e.stopPropagation();
+// });
+
+// removeEventListener
+
+//-------------------------------------------------
+// BOM
+
+// console.log(window.innerHeight);
+// console.log(window.scrollY);
+// window.open("http://google.com", "cours js", "height=600, width=800");
+// window.close()
+
+// Evénements adossés à Window
+// alert("hello");
+
+// confirm
+btn2.addEventListener("click", () => {
+  confirm("Voulez vous vraiment vous tromper ?");
+});
+
+// prompt
+btn1.addEventListener("click", () => {
+  let answer = prompt("Entrez votre nom !");
+
+  questionContainer.innerHTML += "<h3>Bravo " + answer + "</h3>";
+});
+
+// Timer, compte à rebours
+setTimeout(() => {
+  questionContainer.style.borderRadius = "300px";
+}, 2000);
+
+// let interval = setInterval(() => {
+//   document.body.innerHTML += `
+//       <div class='box'>
+//         <h2>Nouvelle Boite !</h2>
+//       </div>
+//     `;
+// }, 4000);
+
+document.body.addEventListener("click", (e) => {
+  e.target.remove();
+  clearInterval(interval);
+});
+
+// Location
+// console.log(location.href);
+// console.log(location.host);
+// console.log(location.pathname);
+// console.log(location.search);
+// location.replace("http://lequipe.fr");
+
+// window.onload = () => {
+//   location.href = "http://twitter.fr";
+// };
+
+// Navigator
+// console.log(navigator.userAgent);
+
+// https://developer.mozilla.org/fr/docs/Web/API/Geolocation/getCurrentPosition
+
+// var options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0,
+// };
+
+// function success(pos) {
+//   var crd = pos.coords;
+
+//   console.log("Votre position actuelle est :");
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude : ${crd.longitude}`);
+//   console.log(`La précision est de ${crd.accuracy} mètres.`);
+// }
+
+// function error(err) {
+//   console.warn(`ERREUR (${err.code}): ${err.message}`);
+// }
+
+// navigator.geolocation.getCurrentPosition(success, error, options);
+
+// History
+// console.log(history);
+// window.history.back();
+// history.go(-2)
+
+//------------------------------------------------
+// SetProperty
+window.addEventListener("mousemove", (e) => {
+  nav.style.setProperty("--x", e.layerX + "px");
+  nav.style.setProperty("--y", e.layerY + "px");
 });
